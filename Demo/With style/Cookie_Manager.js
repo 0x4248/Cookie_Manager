e5b70b02e0aeac9b563870705dd057221dc06991
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-05-15 13:02:11 +0100

    Compatibilaty update

---
+++ Demo/With style/Cookie_Manager.js
@@ -0,0 +1,80 @@
+//GNU 3.0 Licence
+//======================COOKIE_MANAGER=========================
+//go import me using "<script type="text/javascript" src="Cookie_Manager.js"></script>"
+//[CONFIG]
+var Say_hello = true    //says hello
+var Use_debug = true    //shows debug info
+//[SCRIPT]
+var ver = 1.1
+
+if(Say_hello = true){
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    console.log("%c[COOKIE_MANAGER] "+"%c["+h+":"+m+":"+s+"] Started Script and listening for Fn call [V"+ver+"]","color: rgb(0, 255, 94);","color: auto;")
+}
+
+
+
+function setCookie(name,value,days) {
+    if(Use_debug == true){
+            var today = new Date();
+            var h = today.getHours();
+            var m = today.getMinutes();
+            var s = today.getSeconds();
+            console.log("%c[COOKIE_MANAGER] "+"%c["+h+":"+m+":"+s+"] Starting to Set cookie","color: rgb(0, 255, 94);","color: auto;")
+            console.log("%c[COOKIE_MANAGER] "+"%c["+h+":"+m+":"+s+"] Cookie_name:"+name+" Data:"+value+" Expires:"+days,"color: rgb(0, 255, 94);","color: auto;")  
+    }
+
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    if(Use_debug == true){
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        console.log("%c[COOKIE_MANAGER] "+"%c["+h+":"+m+":"+s+"] Cookie_name:"+name+" has been set:","color: rgb(0, 255, 94);","color: auto;") 
+    }
+}
+
+function getCookie(name) {
+    if(Use_debug == true){
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        console.log("%c[COOKIE_MANAGER] "+"%c["+h+":"+m+":"+s+"] Reading cookie: "+name,"color: rgb(0, 255, 94);","color: auto;")
+    }
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) { 
+    if(Use_debug == true){
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();  
+        console.log("%c[COOKIE_MANAGER] "+"%c["+h+":"+m+":"+s+"] Removeing cookie:"+name,"color: rgb(0, 255, 94);","color: auto;")
+    }
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    if(Use_debug == true){
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        console.log("%c[COOKIE_MANAGER] "+"%c["+h+":"+m+":"+s+"] The cookie:"+name+" no longer exists","color: rgb(0, 255, 94);","color: auto;")
+    }
+}
